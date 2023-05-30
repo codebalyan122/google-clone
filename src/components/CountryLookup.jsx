44062ea980b4949356cd1c3542755031e77f2350
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2023-05-31 02:05:17 +0530

    added footer page

---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function CountryLookup() {
+  const [country, setCountry] = useState("");
+  const [loading, setLoading] = useState("false");
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+
+      const res = await fetch("https://geolocation-db.com/json/");
+      const data = await res.json();
+      setCountry(data.country_name);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <img src="spinner.svg" className="h-6 w-6" alt="loading" />
+      ) : (
+        country
+      )}
+    </div>
+  );
+}

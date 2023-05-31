1393399243a23c440d95a5bbaa5e7837abd6af4c
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2023-06-01 01:29:36 +0530

    added search box functionality

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchBox() {
+  const searchParams = useSearchParams();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm] = useState(searchTerm || "");
+  const router = useRouter();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!term.trim()) return;
+    router.push(`search/web?searchTerm=${term}`);
+  }
+
   return (
-    <div>
-      Search
-    </div>
-  )
+    <form
+      onSubmit={handleSubmit}
+      className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 items-center ml-10 mr-5 flex-grow max-w-3xl "
+    >
+      <input
+        type="text"
+        className="w-full focus:outline-none"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+        placeholder="Search Google"
+      />
+      <RxCross2
+        onClick={() => setTerm("")}
+        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+      />
+      <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
+      <AiOutlineSearch
+        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+        onClick={(e) => handleSubmit(e)}
+      />
+    </form>
+  );
 }

29fd915396232439e52d1bfc25a7b88f8d1f8611
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2023-06-01 17:19:49 +0530

    created search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,16 +1,21 @@
+import WebSearchResults from "@/src/components/WebSearchResults";
 import Link from "next/link";
 import React from "react";
 
 export default async function WebSearchPage({ searchParams }) {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
   const resposne = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if (!resposne.ok) {
+    throw new Error("something went wrong");
+  }
   const data = await resposne.json();
 
-  const result = data.items;
+  const results = data.items;
 
-  if (!result) {
+  if (!results) {
     return (
       <div className="flex flex-col justify-center items-center pt-4 ">
         <h1 className="text-3xl mb-4">No Result found</h1>
@@ -23,5 +28,5 @@ export default async function WebSearchPage({ searchParams }) {
       </div>
     );
   }
-  return <>{result && result.map((item) => <h1>{item.title}</h1>)}</>;
+  return <>{results && <WebSearchResults results={data} />}</>;
 }

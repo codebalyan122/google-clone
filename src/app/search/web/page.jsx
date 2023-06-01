98ffa611fd0a134ecac13a96a9bd04d05aae1ca4
shivam balayan <shivambalayan@shivams-MacBook-Pro.local>
2023-06-01 09:38:05 +0530

    added search result using google search api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function WebSearchPage() {
-  return <div>webPage</div>;
+export default async function WebSearchPage({ searchParams }) {
+  const resposne = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await resposne.json();
+
+  const result = data.items;
+  return <>{result && result.map((item) => <h1>{item.title}</h1>)}</>;
 }
